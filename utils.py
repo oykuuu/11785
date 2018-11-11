6a619a2a48c2cfe29d845572a1e28a9460908850
harlin <harlin@cmu.edu>
2018-11-11 00:00:29 -0500

    code for testing cnn and ppd components

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ import keras
 import numpy as np
 import tensorflow as tf
 from keras.models import Model
-from keras.layers import Dense, Flatten, Input, Lambda
+from keras.layers import Dense, Flatten, Input, Lambda, Conv2D, MaxPooling2D, Dropout
 
 def get_dataset(name='mnist'):
     if name == 'mnist':
@@ -40,7 +40,8 @@ def permute_pixels(images, seed):
             permuted_images[idx, :, :, channel] = img.reshape((img_r, img_c))
     return permuted_images
 
-def get_ppd_model(name='mnist'):
+
+def get_ppd_model(name='mnist', ppd=True):
     if name == 'mnist':
         input_shape = (28, 28, 1)
     else:
@@ -52,10 +53,75 @@ def get_ppd_model(name='mnist'):
         return phase
     
     input_tensor = Input(name='input_images', shape=input_shape, dtype='float32')
-    inner = Lambda(pixel2phase, name='pixel2phase', output_shape=input_shape)(input_tensor)
-    inner = Flatten(name='flatten')(inner)
+    
+    if ppd:
+        inner = Lambda(pixel2phase, name='pixel2phase', output_shape=input_shape)(input_tensor)
+        inner = Flatten(name='flatten')(inner)
+    else:
+        inner = Flatten(name='flatten')(input_tensor)
     inner = Dense(800,  name='dense800', kernel_initializer='he_normal', activation='relu')(inner)
     inner = Dense(300,  name='dense300', kernel_initializer='he_normal', activation='relu')(inner)
+    y_pred = Dense(10,  name='dense10', kernel_initializer='he_normal', activation='softmax')(inner)
+
+    return Model(inputs=[input_tensor], outputs=y_pred)
+
+
+def get_cnn_model(name='mnist', ppd=True):
+    if name == 'mnist':
+        return mnist_cnn_model(ppd)
+    else:
+        return cifar10_cnn_model(ppd)
+
+    
+def mnist_cnn_model(ppd=True):
+    input_shape = (28, 28, 1)
+   
+    def pixel2phase(images):
+        img_fft = tf.fft2d(tf.cast(images, tf.complex64))
+        phase = tf.angle(img_fft)
+        return phase
+    
+    input_tensor = Input(name='input_images', shape=input_shape, dtype='float32')
+    if ppd:
+        inner = Lambda(pixel2phase, name='pixel2phase', output_shape=input_shape)(input_tensor)
+        inner = Conv2D(32, kernel_size=(3, 3), activation='relu')(inner)
+    else:
+        inner = Conv2D(32, kernel_size=(3, 3), activation='relu')(input_tensor)
+    inner = Conv2D(64, (3, 3), activation='relu')(inner)
+    inner = MaxPooling2D(pool_size=(2, 2))(inner)
+    inner = Dropout(0.25)(inner)
+    inner = Flatten(name='flatten')(inner)
+    inner = Dense(128, activation='relu')(inner)
+    inner = Dropout(0.5)(inner)
+    y_pred = Dense(10,  name='dense10', kernel_initializer='he_normal', activation='softmax')(inner)
+
+    return Model(inputs=[input_tensor], outputs=y_pred)
+
+
+def cifar10_cnn_model(ppd=True):
+    input_shape = (32, 32, 3)  # cifar10
+
+    def pixel2phase(images):
+        img_fft = tf.fft2d(tf.cast(images, tf.complex64))
+        phase = tf.angle(img_fft)
+        return phase
+    
+    input_tensor = Input(name='input_images', shape=input_shape, dtype='float32')
+    if ppd:
+        inner = Lambda(pixel2phase, name='pixel2phase', output_shape=input_shape)(input_tensor)
+        inner = Conv2D(32, (3, 3), padding='same', activation='relu')(inner)
+    else:
+        inner = Conv2D(32, (3, 3), padding='same', activation='relu')(input_tensor)
+    inner = Conv2D(32, (3, 3), activation='relu')(inner)
+    inner = MaxPooling2D(pool_size=(2, 2))(inner)
+    inner = Dropout(0.25)(inner)
+    inner = Conv2D(64, (3, 3), padding='same', activation='relu')(inner)
+    inner = Conv2D(64, (3, 3), padding='same', activation='relu')(inner)
+    inner = MaxPooling2D(pool_size=(2, 2))(inner)
+    inner = Dropout(0.25)(inner)
+    inner = Flatten(name='flatten')(inner)
+    inner = Dense(512, activation='relu')(inner)
+    inner = Dropout(0.5)(inner)
     y_pred = Dense(10,  name='dense10', kernel_initializer='he_normal', activation='softmax')(inner)
 
     return Model(inputs=[input_tensor], outputs=y_pred)
