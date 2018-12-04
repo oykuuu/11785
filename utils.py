d7094b7fd12b4828f0e88680b00cf8e568129bd0
oykuuu <han.oyku@gmail.com>
2018-12-03 22:01:08 -0500

    Hidden phase for CNN

--- utils.py
+++ utils.py
@@ -11,6 +11,8 @@ import tensorflow as tf
 from keras.models import Model
 from keras.layers import Dense, Flatten, Input, Lambda, Conv2D, MaxPooling2D, Dropout
 
+
+
 def get_dataset(name='mnist'):
     if name == 'mnist':
         dataset = keras.datasets.mnist
@@ -66,40 +68,85 @@ def get_ppd_model(name='mnist', ppd=True):
     return Model(inputs=[input_tensor], outputs=y_pred)
 
 
-def get_cnn_model(name='mnist', ppd=True):
+# def get_ppd_model(name='mnist', ppd=True):
+#     if name == 'mnist':
+#         input_shape = (28, 28, 1)
+#     else:
+#         input_shape = (32, 32, 3)  # cifar10
+
+#     def pixel2phase(images):
+#         img_fft = tf.fft(tf.cast(images, tf.complex64))
+#         phase = tf.angle(img_fft)
+#         return phase
+    
+#     input_tensor = Input(name='input_images', shape=input_shape, dtype='float32')
+    
+    
+#     inner = Flatten(name='flatten')(input_tensor)
+#     inner = Dense(800,  name='dense800', kernel_initializer='he_normal', activation='relu')(inner)
+    
+#     if ppd:
+#         inner = Lambda(pixel2phase, name='pixel2phase')(inner)
+#         #inner = Flatten(name='flatten')(inner)
+        
+#     inner = Dense(300,  name='dense300', kernel_initializer='he_normal', activation='relu')(inner)
+#     y_pred = Dense(10,  name='dense10', kernel_initializer='he_normal', activation='softmax')(inner)
+
+#     return Model(inputs=[input_tensor], outputs=y_pred)
+
+
+def get_cnn_model(name='mnist', ppd=True, layerno=0):
     if name == 'mnist':
-        return mnist_cnn_model(ppd)
+        return mnist_cnn_model(ppd, layerno=layerno)
     else:
-        return cifar10_cnn_model(ppd)
+        return cifar10_cnn_model(ppd, layerno=layerno)
 
     
-def mnist_cnn_model(ppd=True):
+def mnist_cnn_model(ppd=True, layerno=0):
     input_shape = (28, 28, 1)
    
+    def pixel2phase1d(images):
+        img_fft = tf.fft(tf.cast(images, tf.complex64))
+        phase = tf.angle(img_fft)
+        return phase
+
     def pixel2phase(images):
         img_fft = tf.fft2d(tf.cast(images, tf.complex64))
         phase = tf.angle(img_fft)
         return phase
     
     input_tensor = Input(name='input_images', shape=input_shape, dtype='float32')
-    if ppd:
+    if ppd and layerno==0:
         inner = Lambda(pixel2phase, name='pixel2phase', output_shape=input_shape)(input_tensor)
         inner = Conv2D(32, kernel_size=(3, 3), activation='relu')(inner)
     else:
         inner = Conv2D(32, kernel_size=(3, 3), activation='relu')(input_tensor)
+    
+    if ppd and layerno==1:
+        inner = Lambda(pixel2phase1d, name='pixel2phase1d')(inner)
+    
     inner = Conv2D(64, (3, 3), activation='relu')(inner)
+    if ppd and layerno==2:
+        inner = Lambda(pixel2phase1d, name='pixel2phase1d')(inner)
     inner = MaxPooling2D(pool_size=(2, 2))(inner)
     inner = Dropout(0.25)(inner)
     inner = Flatten(name='flatten')(inner)
     inner = Dense(128, activation='relu')(inner)
+    if ppd and layerno==3:
+        inner = Lambda(pixel2phase1d, name='pixel2phase1d')(inner)
     inner = Dropout(0.5)(inner)
     y_pred = Dense(10,  name='dense10', kernel_initializer='he_normal', activation='softmax')(inner)
 
     return Model(inputs=[input_tensor], outputs=y_pred)
 
 
-def cifar10_cnn_model(ppd=True):
+def cifar10_cnn_model(ppd=True, layerno=0):
     input_shape = (32, 32, 3)  # cifar10
+
+    def pixel2phase1d(images):
+        img_fft = tf.fft(tf.cast(images, tf.complex64))
+        phase = tf.angle(img_fft)
+        return phase
 
     def pixel2phase(images):
         img_fft = tf.fft2d(tf.cast(images, tf.complex64))
@@ -107,20 +154,30 @@ def cifar10_cnn_model(ppd=True):
         return phase
     
     input_tensor = Input(name='input_images', shape=input_shape, dtype='float32')
-    if ppd:
+    if ppd and layerno==0:
         inner = Lambda(pixel2phase, name='pixel2phase', output_shape=input_shape)(input_tensor)
         inner = Conv2D(32, (3, 3), padding='same', activation='relu')(inner)
     else:
         inner = Conv2D(32, (3, 3), padding='same', activation='relu')(input_tensor)
+    if ppd and layerno==1:
+        inner = Lambda(pixel2phase1d, name='pixel2phase1d')(inner)
     inner = Conv2D(32, (3, 3), activation='relu')(inner)
+    if ppd and layerno==2:
+        inner = Lambda(pixel2phase1d, name='pixel2phase1d')(inner)
     inner = MaxPooling2D(pool_size=(2, 2))(inner)
     inner = Dropout(0.25)(inner)
     inner = Conv2D(64, (3, 3), padding='same', activation='relu')(inner)
+    if ppd and layerno==3:
+        inner = Lambda(pixel2phase1d, name='pixel2phase1d')(inner)
     inner = Conv2D(64, (3, 3), padding='same', activation='relu')(inner)
+    if ppd and layerno==4:
+        inner = Lambda(pixel2phase1d, name='pixel2phase1d')(inner)
     inner = MaxPooling2D(pool_size=(2, 2))(inner)
     inner = Dropout(0.25)(inner)
     inner = Flatten(name='flatten')(inner)
     inner = Dense(512, activation='relu')(inner)
+    if ppd and layerno==5:
+        inner = Lambda(pixel2phase1d, name='pixel2phase1d')(inner)
     inner = Dropout(0.5)(inner)
     y_pred = Dense(10,  name='dense10', kernel_initializer='he_normal', activation='softmax')(inner)
 
